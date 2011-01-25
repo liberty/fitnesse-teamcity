377d1fb6e86c9ea069ab5c7ec1c2a5857c0e23cf
Jacques Morel <jmorel@linuxjoy.(none)>
2011-01-24 19:32:39 -0600

    Added &exit to the test responder url to force fitnesse to exit after the run is done. This is so IDEA can report coverage properly

--- src/main/java/fitnesse/teamcity/TestResponder.java
+++ src/main/java/fitnesse/teamcity/TestResponder.java
@@ -4,4 +4,11 @@ public class TestResponder extends fitnesse.responders.run.TestResponder {
    public TestResponder() {
       formatters.add(new TeamCityLoggingFixtureListener());
    }
+
+   protected void doSending() throws Exception {
+      super.doSending();
+      if (request.hasInput("exit"))
+         System.exit(formatters.getErrorCount());
+   }
+
 }
